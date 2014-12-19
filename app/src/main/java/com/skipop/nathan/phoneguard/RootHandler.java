afd56d02c7d1e1a21af34c37c0f04cb32edae5a6
Nathan Prat <nathan.prat@gmail.com>
2014-12-19 15:29:26 +0100

    RootHandler: code is now calling execCmdSu to handle commands

--- app/src/main/java/com/skipop/nathan/phoneguard/RootHandler.java
+++ app/src/main/java/com/skipop/nathan/phoneguard/RootHandler.java
@@ -109,116 +109,51 @@ class RootHandler {
         }
     }
 
+
     private void cleanSystem(){
-        try {
-            Log.d(tag, "cleanSystem ");
+        Log.d(tag, "cleanSystem ");
 
-            Runtime rt = Runtime.getRuntime();
-            Process proc;
-
-            String command = "su -c rm /system/priv-app/com.skipop.* ";
-            Log.d(tag, "command: " + command);
-            proc = rt.exec(command);
-
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(proc.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(proc.getErrorStream()));
-
-            // read the output from the command
-            String s;
-            while ((s = stdInput.readLine()) != null) {
-                Log.d(tag, "output: " + s);
-            }
-
-            // read any errors from the attempted command
-            while ((s = stdError.readLine()) != null) {
-                Log.d(tag, "error output: " + s);
-            }
-        } catch (Exception e) {
-            Log.d(tag, "Error [" + e.getClass().getName() + "] : " + e.getMessage());
-        }
+        String command = "rm /system/priv-app/com.skipop.* ";
+        ArrayList<String> output = new ArrayList<String>();
+        execCmdSu(command,output);
     }
 
     private void changePermissions(){
-        try {
-            Log.d(tag, "remountSystem ");
+        Log.d(tag, "changePermissions ");
 
-            Runtime rt = Runtime.getRuntime();
-            Process proc;
+        ArrayList<String> output = new ArrayList<String>();
 
-            //update newApk path
-            String[] path = mOldApk.split("/");
-            mNewApk = mNewApk+path[3];
-            Log.d(tag, "newapk: " + mNewApk);
+        //update newApk path
+        String[] path = mOldApk.split("/");
+        mNewApk = mNewApk+path[3];
+        Log.d(tag, "newapk: " + mNewApk);
 
-            String command = "su -c chmod 644 "+mNewApk;
-            Log.d(tag, "command: " + command);
-            proc = rt.exec(command);
+        String command = "su -c chmod 644 "+mNewApk;
 
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(proc.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(proc.getErrorStream()));
-
-            // read the output from the command
-            String s;
-            while ((s = stdInput.readLine()) != null) {
-                Log.d(tag, "output: " + s);
-            }
-
-            // read any errors from the attempted command
-            while ((s = stdError.readLine()) != null) {
-                Log.d(tag, "error output: " + s);
-            }
-        } catch (Exception e) {
-            Log.d(tag, "Error [" + e.getClass().getName() + "] : " + e.getMessage());
-        }
+        execCmdSu(command,output);
     }
 
     private String getMemblockSystem(){
-        try {
-            Log.d(tag, "getMemblockSystem ");
+        //TODO test on real device to adjust
+        Log.d(tag, "getMemblockSystem ");
 
-            Runtime rt = Runtime.getRuntime();
-            Process proc;
-            String command="su -c mount";
-            String fullline,block="";
+        String fullline,block="";
+        ArrayList<String> output = new ArrayList<String>();
+        String command="su -c mount";
 
-            Log.d(tag, "command: " + command);
-            proc = rt.exec(command);
+        execCmdSu(command,output);
 
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(proc.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(proc.getErrorStream()));
-
-            // read the output from the command
-            String s;
-            while ((s = stdInput.readLine()) != null) {
-                if(s.contains("/system")){
-                    fullline = s;
-
-                    String[] tmp = fullline.split(" ",2);
-                    block = tmp[0];
-                    break;
-                }
+        // read the output from the command
+        String s;
+        for(int i=0; i<output.size(); i++){
+            if(output.get(i).contains("system")){
+                String[] tmp = output.get(i).split(" ",2);
+                block = tmp[0];
+                break;
             }
-
-            // read any errors from the attempted command
-            while ((s = stdError.readLine()) != null) {
-                Log.d(tag, "error output: " + s);
-            }
-
-            Log.d(tag, "block: " + block);
-            return block;
-        } catch (Exception e) {
-            Log.d(tag, "Error [" + e.getClass().getName() + "] : " + e.getMessage());
         }
-        return null;
+
+        return block;
     }
 
     private boolean isInstalledSystem(){
@@ -233,135 +168,45 @@ class RootHandler {
     }
 
     private void remountSystem(String block){
-        try {
-            Log.d(tag, "remountSystem ");
+        Log.d(tag, "remountSystem ");
 
-            Runtime rt = Runtime.getRuntime();
-            Process proc;
-            String remount = "su -c mount -o rw,remount "+block+" /system";
+        ArrayList<String> output = new ArrayList<String>();
 
-            Log.d(tag, "command: " + remount);
-            proc = rt.exec(remount);
+        String command = "su -c mount -o rw,remount "+block+" /system";
 
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(proc.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(proc.getErrorStream()));
-
-            // read the output from the command
-            String s;
-            while ((s = stdInput.readLine()) != null) {
-                Log.d(tag, "output: " + s);
-            }
-
-            // read any errors from the attempted command
-            while ((s = stdError.readLine()) != null) {
-                Log.d(tag, "error output: " + s);
-            }
-        } catch (Exception e) {
-            Log.d(tag, "Error [" + e.getClass().getName() + "] : " + e.getMessage());
-        }
+        execCmdSu(command,output);
     }
 
     void eraseOldApk(){
-        try {
-            Log.d(tag, "eraseOldApk ");
+        Log.d(tag, "eraseOldApk ");
 
-            Runtime rt = Runtime.getRuntime();
-            Process proc;
-            String command = "su -c rm "+ mOldApk;
+        ArrayList<String> output = new ArrayList<String>();
 
-            Log.d(tag, "command: " + command);
-            proc = rt.exec(command);
+        String command = "su -c rm "+ mOldApk;
 
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(proc.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(proc.getErrorStream()));
-
-            // read the output from the command
-            String s;
-            while ((s = stdInput.readLine()) != null) {
-                Log.d(tag, "output: " + s);
-            }
-
-            // read any errors from the attempted command
-            while ((s = stdError.readLine()) != null) {
-                Log.d(tag, "error output: " + s);
-            }
-        } catch (Exception e) {
-            Log.d(tag, "Error [" + e.getClass().getName() + "] : " + e.getMessage());
-        }
+        execCmdSu(command,output);
     }
 
     private void copyToSystem(){
         //TODO check setting for root perm
+        Log.d(tag, "copyToSystem ");
 
-        try {
-            Log.d(tag, "copyToSystem ");
+        String command;
+        ArrayList<String> output = new ArrayList<String>();
 
-            Runtime rt = Runtime.getRuntime();
-            Process proc;
-            String command;
-
-            if (Build.VERSION.SDK_INT > 18) {
-                //Android 4.3+ -> /system/priv-app
-                command = "su -c cp "+mOldApk+" /system/priv-app";
-                mNewApk = "/system/priv-app/";
-            }
-            else{
-                // -> /system/app
-                command = "su -c cp "+mOldApk+" /system/app";
-                mNewApk = "/system/app/";
-            }
-            Log.d(tag, "command: " + command);
-            proc = rt.exec(command);
-
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(proc.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(proc.getErrorStream()));
-
-            // read the output from the command
-            String s;
-            while ((s = stdInput.readLine()) != null) {
-                Log.d(tag, "output: " + s);
-
-            }
-
-            // read any errors from the attempted command
-            while ((s = stdError.readLine()) != null) {
-                Log.d(tag, "error output: " + s);
-            }
-        } catch (Exception e) {
-            Log.d(tag, "Error [" + e.getClass().getName() + "] : " + e.getMessage());
+        if (Build.VERSION.SDK_INT > 18) {
+            //Android 4.3+ -> /system/priv-app
+            command = "su -c cp "+mOldApk+" /system/priv-app";
+            mNewApk = "/system/priv-app/";
+        }
+        else{
+            // -> /system/app
+            command = "su -c cp "+mOldApk+" /system/app";
+            mNewApk = "/system/app/";
         }
 
+        execCmdSu(command,output);
     }
-
-    public boolean checkRoot() {
-        boolean isRooted, result2;
-        boolean isApkInstalled = new File("/system/app/Superuser.apk").exists();
-
-        Log.d(tag, "Superuser.apk exists? " + isApkInstalled);
-
-        //result1 = checkMethod1();
-        result2 = checkMethod2();
-
-        isRooted = isApkInstalled && result2;
-
-        if (isRooted)
-            Toast.makeText(mContext, "Root Access granted", Toast.LENGTH_SHORT).show();
-        else
-            Toast.makeText(mContext, "Can't get Root Access", Toast.LENGTH_SHORT).show();
-
-
-        Log.d(tag, "Result: " + isRooted);
-        return isRooted;
-    }//checkRoot
 
     public boolean checkRootv2() {
         //whoami -> no, random -> no
@@ -387,92 +232,4 @@ class RootHandler {
         Log.d(tag, "checkRootv2 isRooted: " + isRooted);
         return isRooted;
     }//checkRootv2
-
-
-// --Commented out by Inspection START (12/18/14 8:18 PM):
-//    private boolean checkMethod1() {
-//        // DOES NOT WORK
-//        boolean isRooted = false;
-//        try {
-//            Log.d(tag, "checkMethod1 ");
-//            //http://stackoverflow.com/questions/5711084/java-runtime-getruntime-getting-output-from-executing-a-command-line-program
-//            Runtime rt = Runtime.getRuntime();
-//            String[] commands = {"su", "-V"};
-//            Process proc = rt.exec(commands);
-//
-//            BufferedReader stdInput = new BufferedReader(new
-//                    InputStreamReader(proc.getInputStream()));
-//
-//            BufferedReader stdError = new BufferedReader(new
-//                    InputStreamReader(proc.getErrorStream()));
-//
-//            // read the output from the command
-//            String s = null;
-//            while ((s = stdInput.readLine()) != null) {
-//                Log.d(tag, "check output: " + s);
-//                try {
-//                    int version = Integer.parseInt(s);
-//                    if (version >= 235)
-//                        isRooted = true;
-//                } catch (NumberFormatException e) {
-//                    //
-//                }
-//            }
-//
-//            // read any errors from the attempted command
-//            while ((s = stdError.readLine()) != null) {
-//                Log.d(tag, "error output: " + s);
-//            }
-//        } catch (Exception e) {
-//            Log.d(tag, "Root access rejected [" + e.getClass().getName() + "] : " + e.getMessage());
-//        }
-//
-//        Log.d(tag, "Method1 result: " + isRooted);
-//        return isRooted;
-//    }//checkMethod1
-// --Commented out by Inspection STOP (12/18/14 8:18 PM)
-
-    private boolean checkMethod2() {
-        // send command "su -c whoami"
-        // seems to be working
-        // -> return true if access was granted, and ask if new install
-        // -> return false is not granted or no su installed
-        boolean isRooted = false;
-        try {
-            Log.d(tag, "checkMethod2 ");
-            //http://stackoverflow.com/questions/5711084/java-runtime-getruntime-getting-output-from-executing-a-command-line-program
-            Runtime rt = Runtime.getRuntime();
-
-            //command su -c -> do not show root prompt
-            //String[] commands = {"su", "-c"};
-            //Process proc = rt.exec(commands);
-            String command = "su -c whoami";
-            Process proc = rt.exec(command);
-
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(proc.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(proc.getErrorStream()));
-
-            // read the output from the command
-            String s;
-            while ((s = stdInput.readLine()) != null) {
-                Log.d(tag, "check output 2: " + s);
-                isRooted = isRooted || s.contains("uid");
-            }
-
-            // read any errors from the attempted command
-            while ((s = stdError.readLine()) != null) {
-                Log.d(tag, "error output 2: " + s);
-                isRooted = isRooted || s.contains("uid");
-            }
-        } catch (Exception e) {
-            Log.d(tag, "Root access rejected [" + e.getClass().getName() + "] : " + e.getMessage());
-        }
-
-        Log.d(tag, "Method2 result: " + isRooted);
-        return isRooted;
-    }//checkMethod2
-
 }//class
